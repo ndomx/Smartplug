183dfc69fb733073e302f423e851b6650c93570b
ndomx <ndominguez@uc.cl>
2018-12-20 00:01:42 -0300

    Update mcu_mqtt/dummy-private-data.h
    Add file description

---
+++ mcu_mqtt/dummy-private-data.h
@@ -0,0 +1,12 @@
+/*
+ * Header file that contains all private data of the communication
+ * including WiFi SSID & password, and MQQT Broker address and
+ * device topic.
+ * REMEMBER TO RENAME THIS FILE TO privte-data.h
+ */
+const char *ssid = "WIFI SSID";
+const char *password = "WIFI PASSWORD";
+
+const char *mqtt_server = "MQTT BROKER ADDRESS";
+const char *topic = "MQTT TOPIC"; 
+// Remember that each device should have its own topic
