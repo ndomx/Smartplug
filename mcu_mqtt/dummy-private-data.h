da2e9266cb1f8f2b3f5cc365252945fd5d67f0f7
ndomx <ndominguez@uc.cl>
2018-12-21 16:44:42 -0300

    Update dummy-private-data.h
    Improved file description

--- mcu_mqtt/dummy-private-data.h
+++ mcu_mqtt/dummy-private-data.h
@@ -8,5 +8,12 @@ const char *ssid = "WIFI SSID";
 const char *password = "WIFI PASSWORD";
 
 const char *mqtt_server = "MQTT BROKER ADDRESS";
-const char *topic = "MQTT TOPIC"; 
-// Remember that each device should have its own topic
+const char *topic = "MQTT TOPIC";
+/*
+ * Each device should have its own topic, i.e.:
+ * home/light1
+ * kitchen/microwave
+ * 
+ * Unless you wish to control more than one plug at
+ * the same time, i.e.: home/all-lights
+ */
